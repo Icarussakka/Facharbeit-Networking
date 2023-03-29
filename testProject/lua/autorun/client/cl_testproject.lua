5910394e668fb06e9fc8b6884774930b1013dba2
kxx <96654450+Icarussakka@users.noreply.github.com>
2023-03-29 20:45:08 +0200

    Add files via upload

---
+++ testProject/lua/autorun/client/cl_testproject.lua
@@ -0,0 +1,51 @@
+local myInt = 24
+
+net.Start("TestProject.MyInt")
+    net.WriteInt(myInt, 32)
+net.SendToServer()
+
+hook.Add("OnPlayerChat", "TestProject.SecureNet", function(ply, text)
+    if text:StartWith("!wep") then
+        net.Start("TestProject.Weapon")
+            net.WriteString(text:sub(6))
+        net.SendToServer()
+    end
+end)
+
+
+// Kommunikation Optimieren
+
+
+myCharacters = {}
+
+net.Receive("TestProject.Characters", function(len)
+    local count = net.ReadUInt(10)
+    for i = 1, count do
+        local id = net.ReadString()
+        local playerName = net.ReadString()
+        local playerHealth = net.ReadInt(8)
+        local playerArmor = net.ReadInt(8)
+        local wepCount = net.ReadInt(10)
+
+        myCharacters[id] = {}
+        myCharacters[id].name = playerName
+        myCharacters[id].health = playerHealth
+        myCharacters[id].armor = playerArmor
+        myCharacters[id].weapons = {}
+
+        for j = 1, wepCount do
+            local wep = net.ReadString()
+
+            myCharacters[id].weapons[wep] = true
+        end
+    end
+
+    print(len)
+    PrintTable(myCharacters)
+end)
+
+net.Receive("ShowTheLength", function(len)
+    local tbl = net.ReadTable()
+
+    print(len)
+end)
---
+++ testProject/lua/autorun/server/sv_testproject.lua
@@ -0,0 +1,81 @@
+util.AddNetworkString("TestProject.MyInt")
+util.AddNetworkString("TestProject.Weapon")
+util.AddNetworkString("TestProject.Characters")
+util.AddNetworkString("ShowTheLength")
+
+// Simple Datentypen Networken
+net.Receive("TestProject.MyInt", function(len, ply)
+    local myInt = net.ReadInt(32)
+
+    print("Received " .. myInt)
+end)
+
+
+// Sicherheit bei der Kommunikation
+
+net.Receive("TestProject.Weapon", function(len, ply)
+    if not ply:IsAdmin() then return end
+
+    local wep = net.ReadString()
+
+    ply:Give(wep)
+end)
+
+
+local characters = {
+    ["PLAYER01"] = {
+        name = "Player 1",
+        health = 100,
+        armor = 0,
+        inventory = {
+            ["weapon_pistol"] = true,
+            ["weapon_smg1"] = true,
+        }
+    },
+    ["PLAYER02"] = {
+        name = "Player 2",
+        health = 100,
+        armor = 0,
+        inventory = {
+            ["weapon_pistol"] = true,
+            ["weapon_smg1"] = true,
+        }
+    },
+    ["PLAYER03"] = {
+        name = "Player 3",
+        health = 100,
+        armor = 0,
+        inventory = {
+            ["weapon_pistol"] = true,
+            ["weapon_smg1"] = true,
+        }
+    },
+    ["Player04"] = {
+        name = "Player 4",
+        health = 100,
+        armor = 0,
+        inventory = {
+            ["weapon_pistol"] = true,
+            ["weapon_smg1"] = true,
+        }
+    }
+}
+
+net.Start("TestProject.Characters")
+    net.WriteInt(table.Count(characters), 10)
+    for id, players in pairs(characters) do
+        net.WriteString(id)
+        net.WriteString(players.name)
+        net.WriteInt(players.health, 8)
+        net.WriteInt(players.armor, 8)
+        net.WriteInt(table.Count(players.inventory), 10)
+        for weapon, _ in pairs(players.inventory) do
+            net.WriteString(weapon)
+        end
+    end
+net.Send(Entity(1))
+
+net.Start("ShowTheLength")
+    net.WriteTable(characters)
+net.Send(Entity(1))
+
